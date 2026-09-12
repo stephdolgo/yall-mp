b236a316e4caeda256f07251bcf53178d10de819
stephdolgo <77083098+stephdolgo@users.noreply.github.com>
2026-09-11 23:45:03 -0400

    Fix Windows postinstall 404: update mpv build link
    20260421 build no longer exists and npm install fails at postinstall on Windows. Updated to the current release, 20260610.

--- scripts/download-dependencies.js
+++ scripts/download-dependencies.js
@@ -18,7 +18,7 @@ const EXTENSIONS_DIR = path.join(RESOURCES_DIR, 'extensions');
 const URLS = {
   yomitan: 'https://github.com/yomidevs/yomitan/releases/download/25.11.11.0/yomitan-chrome.zip',
   win32: {
-    mpv: 'https://github.com/shinchiro/mpv-winbuild-cmake/releases/download/20260421/mpv-x86_64-20260421-git-5921fe5.7z',
+    mpv: 'https://github.com/shinchiro/mpv-winbuild-cmake/releases/download/20260610/mpv-x86_64-20260610-git-304426c.7z',
     audiowaveform: 'https://github.com/bbc/audiowaveform/releases/download/1.10.2/audiowaveform-1.10.2-win64.zip'
   }
 };
